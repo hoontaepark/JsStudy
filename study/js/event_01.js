fcc4edcd21d05e434ee4bce3059aff3f32871bfb
hoontaepark <pthomj13@gmail.com>
2025-03-18 09:59:00 +0900

    eventLoop

---
+++ study/js/event_01.js
@@ -0,0 +1,35 @@
+document.querySelector('.button').addEventListener('click',function(){
+    alert('hello')
+})
+
+console.log(this) //js에서 this를 선언하면 윈도우창을 띄워줌
+
+const btn = document.querySelector('.button')
+btn.addEventListener('click',function(e){ //이벤트루프에서는 요소가 출력이된다. 
+    console.log(e.currentTarget)
+    console.log(e.currentTarget === this) //this는 요소를 가르킴 
+})
+
+const body = document.querySelector('body')
+const div = document.querySelector('div')
+const button = document.querySelector('button')
+
+body.addEventListener('click',function(){ //이벤트 버블링, 버튼클릭시, button -> div -> body순으로 console이 출력
+    console.log('body') //true로 바꿀시, 캡쳐링, 부모 -> 자식 순으로console 출력 
+},true)
+
+body.addEventListener('click', function(){
+    console.log('div')
+})
+
+body.addEventListener('click', function(){
+    console.log('button')
+})
+
+const div2 = document.querySelector('.button2')
+div2.addEventListener('click',function(e){
+    console.log(this)
+})
+
+
+
